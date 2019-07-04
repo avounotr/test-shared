992b4f8b61aa74cb308902bc8e117565c428a950
avounotr <avounotr@gmail.com>
2019-07-05 02:57:04 +0300

    .........

---
+++ src/App2.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App2 extends React.Component {
+  render() {
+    return (
+      <div> Hi </div>
+    );
+  }
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-export { default as App } from './App';
+export { default as App2 } from './App2';
